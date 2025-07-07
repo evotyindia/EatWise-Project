00a748fa60aa9e961c527517df6aff29ceaaa287
Aryan Yadav <arryaan30@gmail.com>
2025-07-07 07:20:27 +0000

    why images are not visble in the  blogs even they are at locations

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,5 +11,5 @@ export const config = {
   // Match all pathnames except for
   // - … if they start with `/api`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico|sitemap.xml|robots.txt).*)']
+  matcher: ['/((?!api|_next/static|_next/image|img|images|favicon.ico|sitemap.xml|robots.txt).*)']
 };
