1a94235cb9ca61bf91f9e479c0e80cea9314c20d
Aryan Yadav <arryaan30@gmail.com>
2025-07-08 12:46:54 +0000

    use rhis color palate and use in a prfesiional way on the whole site and

--- src/components/common/star-rating.tsx
+++ src/components/common/star-rating.tsx
@@ -24,7 +24,7 @@ export function StarRating({
           key={i}
           size={size}
           className={cn(
-            i < Math.round(rating) ? 'text-yellow-400 fill-yellow-400' : 'text-gray-300 dark:text-gray-600',
+            i < Math.round(rating) ? 'text-[color:var(--star)] fill-[color:var(--star)]' : 'text-gray-300 dark:text-gray-600',
             iconClassName
           )}
         />
