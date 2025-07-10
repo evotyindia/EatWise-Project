76879c3c596414f057157d2ee0670a65602d6b7b
Aryan Yadav <arryaan30@gmail.com>
2025-07-10 13:07:02 +0000

    on these kind of dropdowns replace the deperater line slighly below side

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -29,13 +29,15 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium transition-all [&[data-state=open]>svg:last-child]:rotate-180",
+        "flex flex-1 items-center justify-between py-4 font-medium transition-all hover:no-underline [&[data-state=open]>div>svg]:rotate-180",
         className
       )}
       {...props}
     >
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+       <div className="flex h-7 w-7 items-center justify-center rounded-full bg-muted/70 group-hover:bg-muted">
+        <ChevronDown className="h-4 w-4 shrink-0 text-muted-foreground transition-transform duration-200" />
+      </div>
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
